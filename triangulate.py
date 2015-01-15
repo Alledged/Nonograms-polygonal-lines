e87a852e4312e796db9f9c1d69f48997816d01da
Alledged <afubafu@gmail.com>
2015-01-15 14:28:44 +0100

    removed unecessary files'

--- triangulate.py
+++
@@ -1,6 +0,0 @@
-def triangulate(subdivision,animation=None):
-    """Triangulates the given DCEL subdivision.
-
-    animation  a dictionary mapping DCEL features to graphics features
-    """
-    pass
