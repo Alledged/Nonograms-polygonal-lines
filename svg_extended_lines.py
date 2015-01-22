3fa7777a2da7b9724e4218c0804e0ed56e98742a
Alledged <afubafu@gmail.com>
2015-01-22 14:07:00 +0100

    svg_extended_lines.py enables to draw the extended lines to the svg file. Still need to draw the bounding box

--- svg_extended_lines.py
+++ svg_extended_lines.py
@@ -2,6 +2,8 @@
 # drawing and drawing them to an svg file.
 
 import intersections_experiments as ie
+import svgwrite
+import Point
 
 INPUTFILE = "input_files/fat_tangramcat.txt" 
 
@@ -11,12 +13,23 @@ INPUTFILE = "input_files/fat_tangramcat.txt"
 
 edges = ie.parse_file(INPUTFILE)
 all_inter = ie.get_all_legit_intersections(edges)
-print all_inter
 
+print all_inter
 
 
 # create a new drawing function, as the one in draw_dcel
 # draws polygons and i want to draw lines.
-def draw_extended_lines_svg(edges, outputfile):
-	# all_inter is a list of list of intersection points.
-	pass
+def draw_extended_lines_svg(list_inters_points, outputfile):
+	"""draws the extended edges to the outputfile.svg"""
+	dwg = svgwrite.Drawing(filename=outputfile, debug=True)
+	
+	for pts in list_inters_points:
+		print type(pts[0][0])
+		# line = svgwrite.shapes.Line(start=(legit[0][0], legit[0][1]), end=(legit[1][0], legit[1][1]))
+		# print line
+		dwg.add(dwg.line((int(pts[0][0]), int(pts[0][1])), (int(pts[1][0]), int(pts[1][1])), stroke='black'))
+	
+	dwg.save()
+
+
+draw_extended_lines_svg(all_inter, 'output.svg')
