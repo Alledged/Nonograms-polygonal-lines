7d6591177f173c76396c7fb9cfef0ffaa0d4ba37
Alledged <afubafu@gmail.com>
2015-01-27 18:37:58 +0100

    :set et|retab on ploting_dcel.py and draw_dcel.py

--- draw_dcel.py
+++ draw_dcel.py
@@ -5,26 +5,26 @@ import svgwrite
 
 
 def convert_to_points(list_x_y):
-	"""from a tuple of lists ([x1,x2,...], [y1,y1,...]) to a list of [(x1,y1), (x2,y2), ...]"""
-	pts = []
-	for i in range(len(list_x_y[0])):
-		pts.append((list_x_y[0][i], list_x_y[1][i]))
-	return pts
+    """from a tuple of lists ([x1,x2,...], [y1,y1,...]) to a list of [(x1,y1), (x2,y2), ...]"""
+    pts = []
+    for i in range(len(list_x_y[0])):
+        pts.append((list_x_y[0][i], list_x_y[1][i]))
+    return pts
 
 
 def draw(faces, output_file):
-	"""this function draws all faces to the output file given, those part of the figure
-	in black and the others in white"""
-	dwg = svgwrite.Drawing(filename=output_file, debug=True)
-	for face in faces:
-		# vertices is a tuple of list ([x_1,x_2,...], [y_1,y_2,...])
-		if(face.get_ordered_vertices() != None):
-			vertices = convert_to_points(face.get_ordered_vertices())
-			if face.get_is_part_of_drawing() == True:
-				# color black
-				dwg.add(dwg.polygon(vertices, stroke='black', fill='black'))	
-			else:
-				# color white
-				dwg.add(dwg.polygon(vertices, stroke='black', fill='white'))
+    """this function draws all faces to the output file given, those part of the figure
+    in black and the others in white"""
+    dwg = svgwrite.Drawing(filename=output_file, debug=True)
+    for face in faces:
+        # vertices is a tuple of list ([x_1,x_2,...], [y_1,y_2,...])
+        if(face.get_ordered_vertices() != None):
+            vertices = convert_to_points(face.get_ordered_vertices())
+            if face.get_is_part_of_drawing() == True:
+                # color black
+                dwg.add(dwg.polygon(vertices, stroke='black', fill='black'))    
+            else:
+                # color white
+                dwg.add(dwg.polygon(vertices, stroke='black', fill='white'))
 
-	dwg.save()
+    dwg.save()
--- plotting_dcel.py
+++ plotting_dcel.py
@@ -14,93 +14,93 @@ FILENAME = "input_files/points_plane.txt"
 
 # unused method
 # def read_file(file_name):
-# 	"""reads a file and sets points into a list of list"""
-# 	lst_points = []
-# 	f = open(file_name, "r")
-# 	for point in f:
-# 		point = point.strip()
-# 		point = " ".join(point.split()).split()
-# 		point = map(float, point)
-# 		lst_points.append(point)
-# 	return lst_points
+#   """reads a file and sets points into a list of list"""
+#   lst_points = []
+#   f = open(file_name, "r")
+#   for point in f:
+#       point = point.strip()
+#       point = " ".join(point.split()).split()
+#       point = map(float, point)
+#       lst_points.append(point)
+#   return lst_points
 
 
 # unused method
 # def make_points(file_name):
-# 	"""converts a list containing lists of coordinates into a list of Points"""
-# 	l_points = read_file(file_name)
-# 	proper_points = []
-# 	for point in l_points:
-# 		proper_points.append(Point.Point(point))
-# 	return proper_points
-	
+#   """converts a list containing lists of coordinates into a list of Points"""
+#   l_points = read_file(file_name)
+#   proper_points = []
+#   for point in l_points:
+#       proper_points.append(Point.Point(point))
+#   return proper_points
+    
 
 
 # method borrowed from driver.
 def parse_file(file_name):
-	pts = []
-	edges = []
-	for line in file(file_name):
-		data = line.split()
-		if len(data) == 2:
-			try:
-				pts.append(Point.Point(int(data[0]), int(data[1])))
-			except ValueError:
-				print "ignoring line:", line
-		elif len(data) == 4:
-			try:
-				# 4 points in file means edge
-				edges.append((Point.Point(int(data[0]), int(data[1])),
-					Point.Point(int(data[2]), int(data[3]))))
-			except ValueError:
-				print "ingnoring line:", line
+    pts = []
+    edges = []
+    for line in file(file_name):
+        data = line.split()
+        if len(data) == 2:
+            try:
+                pts.append(Point.Point(int(data[0]), int(data[1])))
+            except ValueError:
+                print "ignoring line:", line
+        elif len(data) == 4:
+            try:
+                # 4 points in file means edge
+                edges.append((Point.Point(int(data[0]), int(data[1])),
+                    Point.Point(int(data[2]), int(data[3]))))
+            except ValueError:
+                print "ingnoring line:", line
 
-	if edges:
-		return DCEL.buildGeneralSubdivision(edges, pts)
-	elif pts:
-		return DCEL.buildSimplePolygon(pts)
+    if edges:
+        return DCEL.buildGeneralSubdivision(edges, pts)
+    elif pts:
+        return DCEL.buildSimplePolygon(pts)
 
 
 # def calculate_area(face):
-# 	area = -1
-# 	bounding_vertices_x = []
-# 	bounding_vertices_y = []
+#   area = -1
+#   bounding_vertices_x = []
+#   bounding_vertices_y = []
 
-# 	# identify all points bounding the face in counterclockwise order
-# 	# and store their x and y coordinates in two lists
-# 	edges = face.getOuterBoundary()
-# 	for edge in edges:
-# 		bounding_vertices_x.append(edge.getOrigin().getCoords()[0])
-# 		bounding_vertices_y.append(edge.getOrigin().getCoords()[1])
+#   # identify all points bounding the face in counterclockwise order
+#   # and store their x and y coordinates in two lists
+#   edges = face.getOuterBoundary()
+#   for edge in edges:
+#       bounding_vertices_x.append(edge.getOrigin().getCoords()[0])
+#       bounding_vertices_y.append(edge.getOrigin().getCoords()[1])
 
 
-	
-# 	part_p, part_m = 0, 0
-# 	for i_p, i_m in zip(range(len(bounding_vertices_x)), range(len(bounding_vertices_y))):
-# 		if i_p == len(bounding_vertices_x) - 1:
-# 			part_p += bounding_vertices_x[i_p]*bounding_vertices_y[0]
-# 		else:
-# 			part_p += bounding_vertices_x[i_p]*bounding_vertices_y[i_p+1]
+    
+#   part_p, part_m = 0, 0
+#   for i_p, i_m in zip(range(len(bounding_vertices_x)), range(len(bounding_vertices_y))):
+#       if i_p == len(bounding_vertices_x) - 1:
+#           part_p += bounding_vertices_x[i_p]*bounding_vertices_y[0]
+#       else:
+#           part_p += bounding_vertices_x[i_p]*bounding_vertices_y[i_p+1]
 
-# 		if i_m == len(bounding_vertices_y) - 1:
-# 			part_m += bounding_vertices_x[0]*bounding_vertices_y[i_m]
-# 		else:
-# 			part_m += bounding_vertices_x[i_m + 1]*bounding_vertices_y[i_m]
+#       if i_m == len(bounding_vertices_y) - 1:
+#           part_m += bounding_vertices_x[0]*bounding_vertices_y[i_m]
+#       else:
+#           part_m += bounding_vertices_x[i_m + 1]*bounding_vertices_y[i_m]
 
-	
-# 	total = 0.5 * (part_p - part_m)
-# 	return total
+    
+#   total = 0.5 * (part_p - part_m)
+#   return total
 
 
 def main():
 
-	dcel_new = parse_file(FILENAME)
-	#dcel_new.plot_whole_dcel()
-	#faces = dcel_new.getFaces()
-	dcel_new.plot_whole_dcel()
-	#faces = dcel_new.getFaces()
-	#faces[1].plot_face()
-	
+    dcel_new = parse_file(FILENAME)
+    #dcel_new.plot_whole_dcel()
+    #faces = dcel_new.getFaces()
+    dcel_new.plot_whole_dcel()
+    #faces = dcel_new.getFaces()
+    #faces[1].plot_face()
+    
 
 if __name__ == '__main__':
-	main()
+    main()
